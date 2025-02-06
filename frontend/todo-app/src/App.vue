2e7d9ae238875db09c3e96dacfbcd9a0f46b18e1
vasu744896 <vasanthkumar744896@gmail.com>
2025-02-06 14:05:38 +0530

    Added frontend Vuejsr

---
+++ frontend/todo-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  margin-top: 50px;
+}
+</style>
