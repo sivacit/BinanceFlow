e00b86e88523cec9b4ff07c1c29f24425d3517cc
vasu744896 <vasanthkumar744896@gmail.com>
2025-02-19 21:42:43 +0530

    correction of predicition data

--- frontend/btc_chart/src/router/index.js
+++ frontend/btc_chart/src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import BTCChart from "../components/BTCChart.vue";
 import CandleView from "../components/CandleView.vue";
-import FilterDate from "../components/FilterDate.vue"; // Import the FilterDate component
 
 const routes = [
   { path: "/", name: "BTCChart", component: BTCChart },
   { path: "/candle-view", name: "CandleView", component: CandleView },
-  { path: "/filter-date", name: "FilterDate", component: FilterDate }, // Ensure this route exists
+  { path: "/filter-date", name: "FilterDate", component: () => import("../components/FilterDate.vue") }, // Lazy loading
 ];
 
 const router = createRouter({
